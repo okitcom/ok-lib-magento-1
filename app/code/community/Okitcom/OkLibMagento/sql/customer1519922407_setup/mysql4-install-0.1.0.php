a314707a4abc67fe2d4a64804f647cd90474d24b
Hidde Lycklama <mail.hidde@gmail.com>
2018-03-13 12:35:53 +0100

    Improve install script

--- app/code/community/Okitcom/OkLibMagento/sql/customer1519922407_setup/mysql4-install-0.1.0.php
+++ app/code/community/Okitcom/OkLibMagento/sql/customer1519922407_setup/mysql4-install-0.1.0.php
@@ -2,10 +2,6 @@
 		$installer = $this;
 		$installer->startSetup();
 
-if ($installer->attributeExists("customer", 'oktoken')) {
-    $installer->removeAttribute("customer", 'oktoken');
-}
-
 $installer->addAttribute("customer", "oktoken", array("type"=>"varchar", "visible" => false, "required" => false));
 		$installer->endSetup();
 			 
