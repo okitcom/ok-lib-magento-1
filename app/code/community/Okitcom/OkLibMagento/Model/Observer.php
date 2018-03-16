f01bc3006c3f5603c559823f725c891fd2c4655c
Hidde Lycklama <mail.hidde@gmail.com>
2018-03-16 15:47:55 +0100

    Fix error in cron script

--- app/code/community/Okitcom/OkLibMagento/Model/Observer.php
+++ app/code/community/Okitcom/OkLibMagento/Model/Observer.php
@@ -24,58 +24,64 @@ class Okitcom_OkLibMagento_Model_Observer
     }
 
     public function updateStatus() {
-        Mage::log('Running OK transaction status check');
+            Mage::log('Running OK transaction status check');
 
-        /** @var \OK\Service\Cash $okCash */
-        $okCash = Mage::helper('oklibmagento/oklib')->getCashClient();
-        $transactions = Mage::helper('oklibmagento/checkout')->loadAllPending();
-//        $this->logger->info("Found " . $transactions->count() . " tx to update");
-        $updated = 0;
-        $completed = 0;
-        $still_pending = 0;
-        /** @var Okitcom_OkLibMagento_Model_Checkout $item */
-        foreach ($transactions->getItems() as $item) {
-            // Get status
+            /** @var \OK\Service\Cash $okCash */
+            $okCash = Mage::helper('oklibmagento/oklib')->getCashClient();
+            $transactions = Mage::helper('oklibmagento/checkout')->loadAllPending();
+//            Mage::log("Found " . $transactions->count() . " tx to update");
+            $updated = 0;
+            $completed = 0;
+            $still_pending = 0;
+            /** @var Okitcom_OkLibMagento_Model_Checkout $item */
+            foreach ($transactions->getItems() as $item) {
+                // Get status
 
-            $guid = $item->getGuid();
-            try {
-                $okResponse = $okCash->get($guid);
-                if ($okResponse != null && $okResponse->state != $item->getState()) {
-                    $item->setState($okResponse->state);
-                    $item->save();
+                $guid = $item->getGuid();
+                try {
+                    $okResponse = $okCash->get($guid);
+                    if ($okResponse != null && $okResponse->state != $item->getState()) {
+                        $item->setState($okResponse->state);
+                        $item->save();
 
-                    if ($okResponse->state == Okitcom_OkLibMagento_Helper_Config::STATE_CHECKOUT_SUCCESS) {
-                        $this->createOrder($item, $okResponse);
-                        $completed++;
+                        if ($okResponse->state == Okitcom_OkLibMagento_Helper_Config::STATE_CHECKOUT_SUCCESS) {
+                            $this->createOrder($item, $okResponse);
+                            $completed++;
+                        }
+
+                        $updated++;
+                    } else {
+                        $still_pending++;
                     }
-
-                    $updated++;
-                } else {
-                    $still_pending++;
+                } catch (\Exception $e) {
+                    Mage::log("Could not update OK transaction with id " . $item->getId());
                 }
-            } catch (\Exception $e) {
-                Mage::log("Could not update OK transaction with id " . $item->getId(), "error");
+
+                // Mark NewPendingTrigger transactions as closed (if older than X time)
+                //$this->logger->info("Transaction ID " . $item->id . " state " . $item->state);
             }
 
-            // Mark NewPendingTrigger transactions as closed (if older than X time)
-            //$this->logger->info("Transaction ID " . $item->id . " state " . $item->state);
-        }
-
-        if ($updated > 0) {
-            Mage::log("Ran update on " . $transactions->count() . " transactions. (" . $updated . " updated, " . $completed . " completed, " . $still_pending . " still pending)");
-        }
+            if ($updated > 0) {
+                Mage::log("Ran update on " . $transactions->count() . " transactions. (" . $updated . " updated, " . $completed . " completed, " . $still_pending . " still pending)");
+            }
     }
 
     private function createOrder(Okitcom_OkLibMagento_Model_Checkout $checkout, $okResponse) {
         // process
         if ($checkout->getSalesOrderId() == null) {
             // update
-            $quote = Mage::helper('oklibmagento/checkout')->loadByQuoteId($checkout->loadByQuoteId());
+            $quote = Mage::getModel('sales/quote')
+                ->loadByIdWithoutStore($checkout->getQuoteId());
+            if ($quote->getId() == null) {
+                Mage::logException(new Okitcom_OkLibMagento_Helper_Checkout_Exception("Could not find quote on OK transaction object. Checkout: " . $checkout->getId()));
+                return;
+            }
+
             $order = Mage::helper('oklibmagento/order')->createOrder($quote, $okResponse);
 
             $discountOk = $okResponse->authorisationResult->amount->sub($okResponse->amount);
             $checkout->setDiscount(-$discountOk->getCents());
-            $checkout->setSalesOrderId($order->getEntityId());
+            $checkout->setSalesOrderId($order->getId());
             $checkout->save();
         }
     }
