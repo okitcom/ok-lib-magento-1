a772c2d3c963259a61ece5ae1c7387d4158007da
Hidde Lycklama <mail.hidde@gmail.com>
2018-03-20 11:43:36 +0100

    Hide cancel_after field if ok cash service is disabled Dont run cron script if OK cash service is disabled

--- app/code/community/Okitcom/OkLibMagento/Model/Observer.php
+++ app/code/community/Okitcom/OkLibMagento/Model/Observer.php
@@ -24,73 +24,76 @@ class Okitcom_OkLibMagento_Model_Observer
     }
 
     public function updateStatus() {
-            $this->log('Running OK transaction status check');
+        if (!Mage::helper('oklibmagento/oklib')->isServiceEnabled(Okitcom_OkLibMagento_Helper_Oklib::SERVICE_TYPE_CASH)) {
+            return; // Don't run if OK Cash is disabled
+        }
+        $this->log('Running OK transaction status check');
 
-            /** @var \OK\Service\Cash $okCash */
-            $okCash = Mage::helper('oklibmagento/oklib')->getCashClient();
-            $transactions = Mage::helper('oklibmagento/checkout')->loadAllPending();
-            $this->log("Found " . $transactions->count() . " tx to update");
-            $updated = 0;
-            $completed = 0;
-            $canceled = 0;
-            $still_pending = 0;
-            $cancelAfterMinutes = Mage::helper('oklibmagento/config')->getOkCashValue("cancel_after");
+        /** @var \OK\Service\Cash $okCash */
+        $okCash = Mage::helper('oklibmagento/oklib')->getCashClient();
+        $transactions = Mage::helper('oklibmagento/checkout')->loadAllPending();
+        $this->log("Found " . $transactions->count() . " tx to update");
+        $updated = 0;
+        $completed = 0;
+        $canceled = 0;
+        $still_pending = 0;
+        $cancelAfterMinutes = Mage::helper('oklibmagento/config')->getOkCashValue("cancel_after");
 
-            /** @var Okitcom_OkLibMagento_Model_Checkout $item */
-            foreach ($transactions->getItems() as $item) {
-                // Get status
+        /** @var Okitcom_OkLibMagento_Model_Checkout $item */
+        foreach ($transactions->getItems() as $item) {
+            // Get status
 
-                $guid = $item->getGuid();
-                try {
-                    $okResponse = $okCash->get($guid);
-                    if ($okResponse != null) {
+            $guid = $item->getGuid();
+            try {
+                $okResponse = $okCash->get($guid);
+                if ($okResponse != null) {
 
-                        if ($okResponse->state != $item->getState()) {
-                            $item->setState($okResponse->state);
-                            $item->save();
+                    if ($okResponse->state != $item->getState()) {
+                        $item->setState($okResponse->state);
+                        $item->save();
 
-                            if ($okResponse->state == Okitcom_OkLibMagento_Helper_Config::STATE_CHECKOUT_SUCCESS) {
-                                $this->createOrder($item, $okResponse);
-                                $completed++;
-                            }
-
-                            $updated++;
-                        } else {
-                            $still_pending++;
+                        if ($okResponse->state == Okitcom_OkLibMagento_Helper_Config::STATE_CHECKOUT_SUCCESS) {
+                            $this->createOrder($item, $okResponse);
+                            $completed++;
                         }
 
+                        $updated++;
                     } else {
                         $still_pending++;
                     }
 
-                } catch (\Exception $e) {
-                    Mage::logException($e);
-                    $this->log("Could not update OK transaction with id " . $item->getId() . ". Message: " . $e->getMessage());
+                } else {
+                    $still_pending++;
                 }
 
-                if (isset($cancelAfterMinutes) && $cancelAfterMinutes != 0 && $item->getState() === Okitcom_OkLibMagento_Helper_Config::STATE_CHECKOUT_UNSCANNED) {
-                    $age = time() - strtotime($item->getCreatedAt());
-                    if ($age > $cancelAfterMinutes * 60.0) {
-                        try {
-                            $okCash->cancel($guid);
+            } catch (\Exception $e) {
+                Mage::logException($e);
+                $this->log("Could not update OK transaction with id " . $item->getId() . ". Message: " . $e->getMessage());
+            }
 
-                            $item->setState(Okitcom_OkLibMagento_Helper_Config::STATE_CHECKOUT_CANCELLED);
-                            $item->save();
+            if (isset($cancelAfterMinutes) && $cancelAfterMinutes != 0 && $item->getState() === Okitcom_OkLibMagento_Helper_Config::STATE_CHECKOUT_UNSCANNED) {
+                $age = time() - strtotime($item->getCreatedAt());
+                if ($age > $cancelAfterMinutes * 60.0) {
+                    try {
+                        $okCash->cancel($guid);
 
-                            $canceled++;
-                        } catch (\OK\Model\Network\Exception\NetworkException $exception) {
-                            Mage::logException(new Okitcom_OkLibMagento_Helper_Checkout_Exception("Could not cancel OK transaction: " . $item->getId()));
-                        }
+                        $item->setState(Okitcom_OkLibMagento_Helper_Config::STATE_CHECKOUT_CANCELLED);
+                        $item->save();
+
+                        $canceled++;
+                    } catch (\OK\Model\Network\Exception\NetworkException $exception) {
+                        Mage::logException(new Okitcom_OkLibMagento_Helper_Checkout_Exception("Could not cancel OK transaction: " . $item->getId()));
                     }
                 }
-
-                // Mark NewPendingTrigger transactions as closed (if older than X time)
-                //$this->logger->info("Transaction ID " . $item->id . " state " . $item->state);
             }
 
-            if ($still_pending > 0 || $updated > 0 || $canceled > 0) {
-                $this->log("Ran update on " . $transactions->count() . " transactions. (" . $updated . " updated, " . $completed . " completed, " . $canceled . " canceled, " . $still_pending . " still pending)");
-            }
+            // Mark NewPendingTrigger transactions as closed (if older than X time)
+            //$this->logger->info("Transaction ID " . $item->id . " state " . $item->state);
+        }
+
+        if ($still_pending > 0 || $updated > 0 || $canceled > 0) {
+            $this->log("Ran update on " . $transactions->count() . " transactions. (" . $updated . " updated, " . $completed . " completed, " . $canceled . " canceled, " . $still_pending . " still pending)");
+        }
     }
 
     /**
