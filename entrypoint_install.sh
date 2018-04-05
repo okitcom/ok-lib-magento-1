dad6e65b561054dc3970d825ddf350d6852b2975
Hidde Lycklama <mail.hidde@gmail.com>
2018-04-05 17:23:21 +0200

    Fix running magento configuration php file

--- entrypoint_install.sh
+++ entrypoint_install.sh
@@ -5,6 +5,7 @@ certbot --apache -n --agree-tos --email hidde@okit.com --domains oklibmagento1.o
 /usr/local/bin/install-sampledata
 /usr/local/bin/install-magento
 
+cp /var/www/oklib/SetupConfig.php /var/www/html/SetupConfig.php
 php /var/www/html/SetupConfig.php
 
 /sbin/my_init
